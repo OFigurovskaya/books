83732f02e2faba3190bacaa8548dd43992153c12
Olesya Figurovskaya <lisonkadrug@gmail.com>
2025-04-09 18:27:03 +0300

    button change theme

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,19 @@ import Books from './components/Books.vue';
 import Loader from './components/Loader.vue';
 import GoFavoriteBtn from './components/GoFavoriteBtn.vue';
 import GoUpBtn from './components/GoUpBtn.vue';
+import ChangeThemeBtn from './components/ChangeThemeBtn.vue';
 const booksList = useBooksList();
 
 const toggleTab = (id) => {
   booksList.toggleActiveTab(id)
-}
+};
 
 </script>
 
 <template>
-  <main class="container-fluid">
+  <main class="container-fluid ligth thene">
     <header class="header mb-3" id="ancor2">
+      <ChangeThemeBtn />
       <h2 class="h2 text-info text-center" >My favorite books</h2>
       <div class="tabs d-flex justify-content-center mb-3 gap-4">
         <button :class="['btn', 'btn_lg',  'btn-primary ', { 'btn-success': booksList.activeTab === 1 }]" @click="toggleTab(1)">Все книги</button>
@@ -25,11 +27,11 @@ const toggleTab = (id) => {
     <GoFavoriteBtn />
     <div v-if="booksList.activeTab === 1" id="ancorUp">
       <div>
-        <h5>Количество всех книг: {{ booksList.countBooks() }}</h5>
+        <h5 class="text-info">Количество всех книг: {{ booksList.countBooks() }}</h5>
       <Books  v-for="book of booksList.booksList" :key="book.cover_i" :book="book" />
       </div>
       <div id="ancor" >
-        <h5>Количество избранных книг: {{ booksList.countBooksFilter() }}</h5>
+        <h5 class="text-info">Количество избранных книг: {{ booksList.countBooksFilter() }}</h5>
       <Books  v-for="book of booksList.favoriteBooks" :key="book.cover_i" :book="book" />
       </div>
     </div>
---
+++ src/components/ChangeThemeBtn.vue
@@ -0,0 +1,10 @@
+<template>
+    <button class="changeBtn btn btn_lg btn-dark mt-3 " @click="changeTheme.goChangeTheme">{{ changeTheme.textChangeText }}</button>
+</template>
+
+<script setup>
+import { useChangeTheme } from '../stores/changeThemeBtn';
+const changeTheme = useChangeTheme();
+
+
+</script>
