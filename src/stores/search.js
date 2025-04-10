0d1e1bb1b9598b5f954c65677853a3df247c6c79
Olesya Figurovskaya <lisonkadrug@gmail.com>
2025-04-10 15:27:58 +0300

    styles changed

--- src/stores/search.js
+++ src/stores/search.js
@@ -3,27 +3,34 @@ import { useBooksList } from '../stores/BooksBase'
 import { ref } from 'vue'
 
 export const useSearch = defineStore('search', () => {
-const booksList = useBooksList();
-const searchText = ref('');
-const searchBooks = ref([]);
-const loader = ref(false)
+    const booksList = useBooksList();
+    const searchText = ref('');
+    const searchBooks = ref([]);
+    const loader = ref(false)
+    const errorText = ref('')
 
-const goSearch = () => {
-    loader.value = true;
-    setTimeout(() => { 
-        // console.log(booksList.booksList.filter(el => el.title.includes(searchText.value)));
-        // if(booksList.booksList.filter(el => el.title.includes(searchText.value))) {
-              searchBooks.value =  (booksList.booksList.filter(el => el.title.includes(searchText.value)));
-        loader.value = false;
-        // }
-    }, 1000)
-}
 
+    //поиск книги по имени
+    const goSearch = () => {
+        loader.value = true;
+        setTimeout(() => {
+            searchBooks.value = (booksList.booksList.filter(el => el.title.includes(searchText.value)));
+            errorText.value = '';
+            searchText.value = '';
+            //усли книга не найдена:
+            if (searchBooks.value.length === 0) {
+                loader.value = false;
+                return errorText.value = 'Книга не найдена. Попробуйте поискать другую';
+            }
+            loader.value = false;       
+        }, 1000)
+    }
 
     return {
         goSearch,
         searchText,
         searchBooks,
-        loader
+        loader,
+        errorText
     }
 })
